19aa50a8e83762e15d3090715bcd14aa07fcd319
Aenadon <aenadon@outlook.com>
2017-02-19 13:51:52 +0100

    Working URL check on 'add source'

--- app/src/main/java/aenadon/simplerssfeed/EditSourceActivity.kt
+++ app/src/main/java/aenadon/simplerssfeed/EditSourceActivity.kt
@@ -1,15 +1,25 @@
 package aenadon.simplerssfeed
 
+import android.content.Context
+import android.graphics.Color
+import android.os.AsyncTask
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.widget.EditText
 import android.widget.ListView
-import android.widget.Toast
+import com.rometools.rome.io.ParsingFeedException
+import com.rometools.rome.io.SyndFeedInput
+import com.rometools.rome.io.XmlReader
+import java.net.MalformedURLException
 import java.net.URL
+import java.text.ParseException
 import java.util.*
 
 class EditSourceActivity : AppCompatActivity() {
+
+    lateinit var sharedPrefHelper: SharedPrefHelper
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,7 +27,7 @@ class EditSourceActivity : AppCompatActivity() {
 
         val sourceListView = findViewById(R.id.source_editor_list) as ListView
 
-        val sharedPrefHelper = SharedPrefHelper(this@EditSourceActivity)
+        sharedPrefHelper = SharedPrefHelper(this@EditSourceActivity)
         // make an arraylist out of our list
         val feedSources: ArrayList<String> = ArrayList(sharedPrefHelper.getSourcesFromPrefs().map(URL::toString))
 
@@ -40,8 +50,77 @@ class EditSourceActivity : AppCompatActivity() {
         }
     }
 
+
+
     @Suppress("unused") // this function is assigned as onClick handler in the layout XML
     fun addSource(view: View) {
-        Toast.makeText(this, "It works", Toast.LENGTH_LONG).show()
+        val urlBox = EditText(this@EditSourceActivity)
+        urlBox.maxLines = 1
+        urlBox.hint = "http://www.somewebsite.com/feeds/news.xml"
+        urlBox.setHintTextColor(Color.LTGRAY)
+
+        AlertDialog.Builder(this@EditSourceActivity)
+                .setTitle(getString(R.string.enter_url))
+                .setView(urlBox)
+                .setPositiveButton(android.R.string.ok, { dialogInterface, i ->
+                    ValidateURL(this@EditSourceActivity, sharedPrefHelper).execute(urlBox.text.toString())
+                })
+                .setNegativeButton(android.R.string.cancel, null)
+                .show()
     }
+
+    class ValidateURL(val ctx: Context, val sharedPrefHelper: SharedPrefHelper) : AsyncTask<String, Void, Int>() {
+        val VALID_URL = 0
+        val INVALID_URL = 1
+        val INVALID_RSS = 2
+
+        override fun doInBackground(vararg params: String?): Int {
+            val url: URL
+            try {
+                url = URL(params[0])
+                SyndFeedInput().build(XmlReader(url)) // test parsing to see if successful
+
+            } catch (e: MalformedURLException) {
+                e.printStackTrace()
+                return INVALID_URL
+            } catch (e: ParseException) {
+                e.printStackTrace()
+                return INVALID_URL
+            } catch (e: ParsingFeedException) {
+                e.printStackTrace()
+                return INVALID_RSS
+            }
+            // if no error has been thrown, the URL is a valid feed
+            return VALID_URL
+        }
+
+        override fun onPostExecute(result: Int?) {
+            super.onPostExecute(result)
+            if (result != null && result != VALID_URL) {
+                showErrorDialog(result)
+            }
+        }
+
+        fun showErrorDialog(mode: Int) {
+            val title: String
+            val message: String
+            if (mode == INVALID_URL) {
+                title = ctx.getString(R.string.message_invalid_url_title)
+                message = ctx.getString(R.string.message_invalid_url_text)
+            } else if (mode == INVALID_RSS) {
+                title = ctx.getString(R.string.message_invalid_rss_title)
+                message = ctx.getString(R.string.message_invalid_rss_text)
+            } else {
+                return
+            }
+
+            AlertDialog.Builder(ctx)
+                    .setTitle(title)
+                    .setMessage(message)
+                    .setPositiveButton(android.R.string.ok, null)
+                    .show()
+        }
+    }
+
+
 }
